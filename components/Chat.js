eaf1542fb164bf010e663405e47595d01a1a6ee2
Nathan Alexander <natealexander90@gmail.com>
2023-01-14 10:08:53 -0800

    Updating Chat

--- components/Chat.js
+++ components/Chat.js
@@ -8,7 +8,7 @@ export default function Chat() {
     const user = useUser()
     const [messages, setMessages] = useState([])
     const [message, setMessage] = useState('')
-
+    const [chatEngaged, setChatEngaged] = useState(false)
     async function onSubmit(event) {
         event.preventDefault()
         if (!message) return
@@ -38,6 +38,7 @@ export default function Chat() {
                 bot: true,
             }
             setMessages((prev) => [...prev, responseMessage])
+            setChatEngaged(true)
         } catch (error) {
             // Consider implementing your own error handling logic here
             console.error(error)
@@ -47,20 +48,42 @@ export default function Chat() {
     return (
         <div className='flex flex-col min-h-[65vh]'>
             {!messages.length > 0 ? (
-                <div className='flex-1 my-1 p-4 rounded-md text-xs md:text-md tracking-wide leading-loose'>
-                    Welcome to our AI therapy tool. This innovative technology
-                    uses advanced machine learning algorithms to provide
-                    personalized support and guidance for those looking to
-                    improve their mental health and well-being. Our AI therapist
-                    is available 24/7, providing a confidential and convenient
-                    way to address a wide range of mental health concerns. With
-                    the ability to understand and respond to your unique needs,
-                    our AI therapist is here to help you achieve your mental
-                    health goals. Let's get started on your journey to a happier
-                    and healthier life.
+                <div className='flex-1 my-1 p-4 rounded-md text-xs lg:text-lg tracking-wide leading-loose'>
+                    <p>
+                        Welcome to our AI therapy tool. This innovative
+                        technology uses advanced machine learning algorithms to
+                        provide personalized support and guidance for those
+                        looking to improve their mental health and well-being.
+                        Our AI therapist is available 24/7, providing a
+                        confidential and convenient way to address a wide range
+                        of mental health concerns. With the ability to
+                        understand and respond to your unique needs, our AI
+                        therapist is here to help you achieve your mental health
+                        goals. Let's get started on your journey to a happier
+                        and healthier life.
+                    </p>
+                    <br />
+                    <p>
+                        Please note that our AI therapy tool is not intended to
+                        replace professional medical or psychiatric advice. It
+                        is not suitable for use in emergency or crisis
+                        situations and should not be used as a substitute for
+                        professional diagnosis, treatment, or therapy. If you
+                        are experiencing severe symptoms or are in crisis,
+                        please seek immediate help from a qualified healthcare
+                        professional. The information provided by our AI
+                        therapist is based on the data that it has been trained
+                        on, and cannot account for individual circumstances or
+                        variations. By using this service, you accept that our
+                        company will not be held liable for any damages or harm
+                        resulting from the use of this tool. The final decision
+                        for any treatment should be made by a qualified
+                        healthcare professional. If you have any concerns about
+                        your mental health, please seek professional help.
+                    </p>
                 </div>
             ) : (
-                <div className='flex-1 my-1 p-4 rounded-md text-xs md:text-md'>
+                <div className='flex-1 my-4p-4  text-xs md:text-md min-h-[65vh] max-h-[65vh] overflow-y-scroll border border-b-white border-x-0 border-t-0'>
                     {messages.map((message, index) => (
                         <div
                             key={index}
@@ -82,20 +105,23 @@ export default function Chat() {
                 </div>
             )}
             <div className='mx-auto w-80'>
-                <input
-                    type='text'
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
-                    className='dark:bg-slate-800 rounded-md w-5/6 text-xs md:text-md'
-                    placeholder='How are you feeling today?'
-                />
-                <button
-                    type='submit'
-                    onClick={onSubmit}
-                    className='bg-green-500 p-2 rounded-md  text-xs md:text-md'
-                >
-                    Send
-                </button>
+                <form onSubmit={onSubmit}>
+                    <input
+                        type='text'
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        className='dark:bg-slate-800 rounded-tl-md rounded-bl-md w-5/6 text-xs md:text-md'
+                        placeholder='How are you feeling today?'
+                    />
+                    <button
+                        type='submit'
+                        onClick={onSubmit}
+                        className='bg-green-500 disabled:bg-green-900 p-2 rounded-tr-md rounded-br-md  text-xs md:text-md border'
+                        disabled={message.length < 1}
+                    >
+                        Send
+                    </button>
+                </form>
             </div>
         </div>
     )
